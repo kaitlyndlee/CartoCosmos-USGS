68887eb984e237cac0afc51b8d6e79194426dfc7
Kaitlyn Lee <kdl222@nau.edu>
2019-10-23 08:34:39 -0700

    First round of changes.

--- App/js/planetary/planetaryMap.js
+++ App/js/planetary/planetaryMap.js
@@ -4,47 +4,133 @@
 
 // PlanetaryMap Class extends OL with custom Planetary related functions
 class PlanetaryMap {
-  //var target;
-  this.view;
-  projection;
-  map;
-  baseLayers;
-  overLays;
-  zoom;
 
-  constructor(target, layers) {
+  constructor(target, projection) {
+    this.mapID = 'map'
+    this.target = target;
+    this.view = null;
+    this.projection = 'EPSG:4326';  //default for now
+    this.map = null;
+    this.baseLayers = null;
+    this.overlays = null;
+    this.zoom = null;
+
+    var layers = this.parseWebAtlas();
+    this.createMap(layers);
 
   }
 
-  function makeView(){
+  // makeView() {
+    
+  // }
+
+  createMap(layers) {
     this.view = new ol.View({
       //extent: extent,
       projection: this.projection,
       center: ol.proj.fromLonLat([37.41, 8.82]),
       zoom: this.zoom
     });
+
+    // makeView();
+    
+    this.map = new ol.Map({
+      target: this.mapID,
+      // layers: [this.baseLayers, this.overlays],
+      view: this.view
+    });
+
+    var layerAlreadyVisible = false;
+
+    //TODO: separate into smaller methods?
+    var baseLayers = layers['base'];
+    for(var i = 0; i < baseLayers.length; i++) {
+      var currentLayer = baseLayers[i];
+      // TODO: Get a better understanding of this
+      var layerVisible = currentLayer['primary'];
+      if(layerAlreadyVisible) {
+        layerVisible = false;
+      }
+      else if(!layerAlreadyVisible && layerVisible == 'true') {
+        layerAlreadyVisible = true;
+      }
+      var baseLayer = new ol.layer.Tile({
+        title: currentLayer['displayname'],
+        type: 'base',
+        visible: layerVisible,
+        // maxResolution: computedMaxResolution,
+        source: new ol.source.TileWMS({
+          url: currentLayer['url'] + '?map=' + currentLayer['map'],
+          params:{ 'LAYERS': currentLayer['layer']},
+          serverType: 'mapserver',
+          crossOrigin: 'anonymous',
+          // wrapX: wrapCheck
+        })
+      });
+      this.map.addLayer(baseLayer);
+    }
+
+    var overlays = layers['overlays'];
+    for(var i = 0; i < overlays.length; i++) {
+      var currentLayer = overlays[i];
+
+      var overlay = new ol.layer.Tile({
+        title: currentLayer['displayname'],
+        visible: currentLayer['primary'],
+        // maxResolution: computedMaxResolution,
+        enableOpacitySliders: true,
+        source: new ol.source.TileWMS({
+          url: currentLayer['url'] + '?map=' + currentLayer['map'],
+          params:{ 'LAYERS': currentLayer['layer']},
+          serverType: 'mapserver',
+          crossOrigin: 'anonymous',
+          // wrapX: wrapCheck
+        })
+      });
+      this.map.addOverlay(overlay);
+    }
   }
 
-  function parseWebAtlas(){
-    var targets = myJSONmaps['targets'];
-    bases = [];
-    overs = [];
+// TODO Change this?
+  parseWebAtlas() {
+    var layers = {
+      'base': [],
+      'overlays': [],
+      'wfs': []
+    };
 
-    for(var i = 0, len = targets.length; i < len; i++){
+    var targets = myJSONmaps['targets'];
+    for(var i = 0; i < targets.length; i++) {
       var currentTarget = targets[i];
 
-      console.log(myJSONmaps.parse());
-  }
-}
+      if (currentTarget['name'].toLowerCase() == this.target) {
 
-var planetaryMap = new PlanetaryMap({
-  target: 'map',
-  layers: baseLayers,
-  view: new ol.View({
-    //extent: extent,
-    projection: projection,
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 4
-  })
-});
-planetaryMap.init();
+        var jsonLayers = currentTarget['webmap'];
+        for(var j = 0; j < jsonLayers.length; j++) {
+          var currentLayer = jsonLayers[j];
+          
+          if(currentLayer['type'] == 'WMS') {
+            // Base layer check
+            if(currentLayer['transparent'] == 'false') {
+              layers['base'].push(currentLayer);
+            }
+            else {
+              layers['overlays'].push(currentLayer);
+            }
+          }  
+          else {      
+            layers['wfs'].push(currentLayer);
+          }  
+        }
+      }  
+    }
+    return layers;
+  }
+
+
+
+
+
+
+
+}
